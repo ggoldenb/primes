0a8fb0cff7ec90b4008deaccdc0ad305cc798764
ggoldenb <ggoldenb@gmail.com>
2019-01-26 15:03:46 -0800

    Update test.pl

--- test.pl
+++ test.pl
@@ -4,7 +4,7 @@ use warnings;
 $n=shift or die "$0: big number\n";
 
 `time /root/george/project/primes-eratosthenes.pl $n`; 
-# time ./primes-optimized.pl  $n;
+#### time ./primes-optimized.pl  $n;
 # time ./primes-remember.pl  $n;
 # time ./primes-simple.pl  $n;
 # time ./primes-wheel.pl $n;
